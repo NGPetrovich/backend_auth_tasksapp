cba78ab275d5db0762dbab9c2b5396bbc2ed529a
Nikita Goncharenko <nikita.122495@gmail.com>
2023-02-27 22:02:20 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,10 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const app = express();
+app.use(cors());
 
 connectDB();
-
-const app = express();
-
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
